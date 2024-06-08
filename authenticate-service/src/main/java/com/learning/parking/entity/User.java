d8b44df8892f58d2dccc9f5a078f7920dc87b80d
Ritwik Raj <117512680+rajritwik10@users.noreply.github.com>
2024-06-08 17:15:00 +0530

    added

--- authenticate-service/src/main/java/com/learning/parking/entity/User.java
+++ authenticate-service/src/main/java/com/learning/parking/entity/User.java
@@ -1,5 +1,12 @@
 package com.learning.parking.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class User {
 
+	private String email;
+	private String password;
 }
