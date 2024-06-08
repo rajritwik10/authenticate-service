d64b34385047904222d94a3aa81bb2670d6e0d78
Ritwik Raj <117512680+rajritwik10@users.noreply.github.com>
2024-06-08 16:53:32 +0530

    added

---
+++ authenticate-service/src/main/java/com/learning/parking/AuthenticateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learning.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticateServiceApplication.class, args);
+	}
+
+}
